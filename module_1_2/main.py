21ddd9da4acd3912cffef2dcd110d648bdec86cf
LadaDzenis <lada.tranevskaya@mail.ru>
2024-07-31 17:37:34 +0300

    Add files via upload

---
+++ module_1_2/main.py
@@ -0,0 +1,7 @@
+
+quantity = 12
+watch = 1.5
+name_course = 'Python'
+time_course = watch/quantity
+print("Курс: " + name_course + ", " + "всего задач: " + str(quantity) + ".")
+print("Затрачено часов: " + str(watch) + ", " + "среднее время выполнения: " + str(time_course) + " часа.")
